b4b4fe4c46f89de42ecb43eff7a593a62b343d78
JPLoso <pedrovelosoj5@gmail.com>
2026-06-16 19:41:57 -0300

    Meio do processode de criação da classe usuários

--- Classes/usuarios.py
+++ Classes/usuarios.py
@@ -1,10 +1,29 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 import uuid
 
-class usuarios:
-    def __init__(self, nome, cpf, email, senha):
+
+class usuarios(ABC):
+    def __init__(self, nome, email, senha, perfil):
         self.id:str = str(uuid.uuid4())
         self.nome:str = nome
-        self.cpf:str = cpf
         self.email:str = email
         self.__senha:str = senha
+        self.perfil = perfil
+        
+
+
+
+class cliente(usuarios):
+    def __init__(self, nome, cpf, email, senha, perfil):
+        super().__init__(nome, cpf, email, senha, perfil)
+        self.cpf = cpf
+
+class imobiliaria(usuarios):
+    def __init__(self, nome, cnpj, email, senha, perfil):
+        super().__init__(nome, email, senha, perfil)
+        self.cnpj = cnpj
+
+class forncedor(usuarios):
+    def __init__(self, nome, cnpj, email, senha, perfil):
+        super().__init__(nome, email, senha, perfil)
+        self.cnpj = cnpj
